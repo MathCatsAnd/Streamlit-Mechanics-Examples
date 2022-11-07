3c6289f80896c1f020c4d6c5fc79386ed6610a9d
MathCatsAnd <mathcatsand@gmail.com>
2022-11-07 08:30:31 -0800

    Added Interacting Widget Options
    Added another code example

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ st.write('# Streamlit Code Snippets and Examples')
 
 with st.expander('color selector'):
     st.write('## Using Buttons to Override or Reset Color Pickers')
-    st.write('Originally asked in the Streamlit Forums by user mathcatsand (me!)')
+    st.write('Re: post from Streamlit Forums user mathcatsand (me!)')
     st.write('[Color-Picker Unexpected Behavior]'\
              '(https://discuss.streamlit.io/t/color-picker-unexpected-behavior'\
              '/32380)')
@@ -13,7 +13,7 @@ with st.expander('color selector'):
 
 with st.expander('column selector'):
     st.write('## Selecting Columns for Display')
-    st.write('Originally asked in the Streamlit Forums by user fruitzebra')
+    st.write('Re: post from Streamlit Forums user fruitzebra')
     st.write('[Column selection in the dashboard]'\
              '(https://discuss.streamlit.io/t/column-selection-in-the-'\
              'dashboard/32910)')
@@ -24,7 +24,7 @@ with st.expander('column selector'):
 
 with st.expander('column selector v2'):
     st.write('## Selecting Columns for Display V2')
-    st.write('Originally asked in the Streamlit Forums by user fruitzebra')
+    st.write('Re: post from Streamlit Forums user fruitzebra')
     st.write('[Column selection in the dashboard]'\
              '(https://discuss.streamlit.io/t/column-selection-in-the-'\
              'dashboard/32910)')
@@ -32,9 +32,21 @@ with st.expander('column selector v2'):
              'Instead of using the multiselect widget to specify columns for '\
              'display, checkboxes are used in a two-column display.')
 
+with st.expander('interacting widget options'):
+    st.write('## Restricting Widget Options Depending on Another Widget (Bidirectional)')
+    st.write('Re: post from Streamlit Forums user PatrickBalada')
+    st.write('[Filter dataframe regardless of the order of selection]'\
+             '(https://discuss.streamlit.io/t/filter-dataframe-regardless-of-'\
+             'the-order-of-selection/32948)')
+    st.write('This shows how widget states and options can be affected by '\
+             'other widgets. This example shows two widgets applying a filter '\
+             'to a data frame, where each widget gets impossible options '\
+             'removed depending on the other widget. Warning: this can result '\
+             'in a "dead end" and a reset button is provided for that case.')
+
 with st.expander('relabeling images'):
     st.write('## Relabeling Images')
-    st.write('Originally asked in the Streamlit Forums by user Truong_Dang_Manh')
+    st.write('Re: post from Streamlit Forums user Truong_Dang_Manh')
     st.write('[How to display a list of images in groups of 10/50/100?]'\
              '(https://discuss.streamlit.io/t/how-to-display-a-list-of-images'\
              '-in-groups-of-10-50-100/32935)')
