65389f3f53d78b06b2448003d891e173e076dcc5
MathCatsAnd <mathcatsand@gmail.com>
2022-12-03 11:38:51 -0800

    Jupyter Notebook Example
    Testing deployment of page to execute a jupyter notebook on Streamlit Cloud

---
+++ pages/run_jupyter.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import subprocess
+from bs4 import BeautifulSoup
+
+greeting = st.selectbox('Select a Greeting',['','Hi','Hey','Hello','\'Sup','Greetings','Howdy'])
+name = st.text_input('Name')
+
+if st.button('Submit'):
+    if greeting == '' or name == '':
+        st.write('Please fill in name and greeting')
+    else:
+        inputs = pd.DataFrame({'greeting':[greeting],'name':[name]})
+        inputs.to_csv('files/my_notebook_inputs.csv', index=False)
+        subprocess.run(['jupyter','nbconvert', '--execute', 'files/my_notebook.ipynb', '--to', 'html'])
+        with open('files/my_notebook.html','r', encoding='utf-8') as f:
+            result = BeautifulSoup(f,'html.parser')
+        result = result.prettify()  
+        result = result.removeprefix('<!DOCTYPE html>')
+        st.components.v1.html(result, height=400, scrolling=True)
