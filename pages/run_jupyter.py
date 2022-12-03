0a7105244e97fbaeba7b45567320c17b1a0a353c
MathCatsAnd <mathcatsand@gmail.com>
2022-12-03 11:50:58 -0800

    Simplified HTML rendering
    Removed BeautifulSoup and rendered from a direct read of the html file

--- pages/run_jupyter.py
+++ pages/run_jupyter.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import subprocess
-from bs4 import BeautifulSoup
 
 greeting = st.selectbox('Select a Greeting',['','Hi','Hey','Hello','\'Sup','Greetings','Howdy'])
 name = st.text_input('Name')
@@ -14,7 +13,6 @@ if st.button('Submit'):
         inputs.to_csv('files/my_notebook_inputs.csv', index=False)
         subprocess.run(['jupyter','nbconvert', '--execute', 'files/my_notebook.ipynb', '--to', 'html'])
         with open('files/my_notebook.html','r', encoding='utf-8') as f:
-            result = BeautifulSoup(f,'html.parser')
-        result = result.prettify()  
+            result = f.read()
         result = result.removeprefix('<!DOCTYPE html>')
         st.components.v1.html(result, height=400, scrolling=True)
