921126aa20c2a7ec149767d49a4711061757eaf4
MathCatsAnd <mathcatsand@gmail.com>
2022-12-05 18:42:31 -0800

    Additional logging options
    Added more logging options for testing of remote script to run app.

--- pages/run_by_script.py
+++ pages/run_by_script.py
@@ -7,10 +7,19 @@ try:
 except:
     log = pd.DataFrame({'Run Time':[]})
 
-now = pd.DataFrame({'Run Time':[str(datetime.now())]})
+if 'logged' not in st.session_state:
+    st.session_state.logged = False
 
-log = pd.concat([log,now], axis=0, ignore_index=True)
+if st.session_state.logged == False:
+    st.session_state.logged = True
+    now = pd.DataFrame({'Run Time':[str(datetime.now())]})
+    log = pd.concat([log,now], axis=0, ignore_index=True)
+    log.tail().to_csv('files/run_by_script_log.csv', index=False) 
 
 log
 
-log.tail().to_csv('files/run_by_script_log.csv', index=False) 
+def again():
+    st.session_state.logged = False
+
+st.button('Log Another Time', on_click=again)
+st.button('Refresh Logs')
