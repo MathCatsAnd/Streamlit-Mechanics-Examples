0a22dd2b308a7b9877789ac674f38522f43a956b
MathCatsAnd <mathcatsand@gmail.com>
2022-12-05 19:07:04 -0800

    Added remote script to run app
    Added description of remote script that can be used to run the page.

--- pages/run_by_script.py
+++ pages/run_by_script.py
@@ -23,3 +23,21 @@ def again():
 
 st.button('Log Another Time', on_click=again)
 st.button('Refresh Logs')
+
+st.markdown('This page can be scheduled to execute remotely by script using '\
+    'Selenium with Chromedriver. If an app has an expensive caching operation '\
+    'to perform that is shared between sessions, this is one way to prompt the '\
+    'app to run.')
+
+st.code('''
+from selenium import webdriver
+import time
+
+url = 'https://mathcatsand-examples.streamlit.app/run_by_script'
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+browser = webdriver.Chrome(options=options)
+page = browser.get(url)
+time.sleep(10)
+browser.quit()
+''')
