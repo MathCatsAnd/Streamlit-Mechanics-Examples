d8aa096962dd58e3aab5bd45b883c514a880bd24
MathCatsAnd <98661771+MathCatsAnd@users.noreply.github.com>
2023-01-03 18:27:00 -0800

    Updated Background Page

--- pages/background.py
+++ pages/background.py
@@ -1,21 +1,17 @@
 import streamlit as st
-from pathlib import Path
 
-p = Path.cwd() / 'files/cat_background.jpg'
-
-st.write(p)
-
-css = f'''
-.stApp {{
-    background-image: url('{p}');
-}}
-.stApp > header {{
+css = '''
+.stApp {
+    background: url('https://raw.githubusercontent.com/MathCatsAnd/Streamlit-Mechanics-Examples/main/files/cat_background.jpg');
+}
+.stApp > header {
     background-color: transparent;
-}}
+}
 
 '''
-st.image('./files/cat_background.jpg')
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-'<a href="https://www.vecteezy.com/free-vector/cat-pattern">Cat Pattern Vectors by Vecteezy</a>'
+st.write('<div style="background-color: grey; font-size: 3em">Image attribution:<br />' \
+    '<a href="https://www.vecteezy.com/vector-art/5298806-seamless-pattern-of-cute-cartoon-cat-illustration-design-on-purple-background">Artwork by psmxp</a><br/>' \
+    '<a href="https://www.vecteezy.com/free-vector/cat-pattern">Cat Pattern Vectors by Vecteezy</a></div>',unsafe_allow_html=True)
 
