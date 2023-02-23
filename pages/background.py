741fee4475ece4e06785cc2d35cb27f3b22dce72
MathCatsAnd <98661771+MathCatsAnd@users.noreply.github.com>
2023-02-22 17:06:53 -0800

    Updated Background Example
    Updated background example to reflect static file serving available in Streamlit 1.18.0

--- pages/background.py
+++ pages/background.py
@@ -1,33 +1,32 @@
 import streamlit as st
-import mimetypes
-from streamlit import runtime
-from streamlit.runtime import caching
 
-image = './files/cat_background.jpg'
-image_id = 'please_do_not_crash'
+choice = st.radio('Background Type', ['Image', 'Gradient'])
 
-# Copied from Streamlit's implementation of st.image
-def serve_image(image, image_id):
-    mimetype, _ = mimetypes.guess_type(image)
-    if mimetype is None:
-        mimetype = "application/octet-stream"
-    url = runtime.get_instance().media_file_mgr.add(image, mimetype, image_id)
-    caching.save_media_data(image, mimetype, image_id)
-    return(url)
+if choice == 'Image':
+    st.title('Image Background')
+    st.markdown('Boop! :ghost:')
 
-url = serve_image(image, image_id)
+    image = './app/static/house-panther-nose.png'
 
-css = f'''
-.stApp {{
-    background-image: url(.{url});
-}}
-.stApp > header {{
-    background-color: transparent;
-}}
-'''
-st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+    css = f'''
+    <style>
+        .stApp {{
+            background-image: url({image});
+            background-size: cover;
 
-st.write('<div style="background-color: grey; font-size: 3em">Image attribution:<br />' \
-    '<a href="https://www.vecteezy.com/vector-art/5298806-seamless-pattern-of-cute-cartoon-cat-illustration-design-on-purple-background">Artwork by psmxp</a><br/>' \
-    '<a href="https://www.vecteezy.com/free-vector/cat-pattern">Cat Pattern Vectors by Vecteezy</a></div>',unsafe_allow_html=True)
+        }}
+        .stApp > header {{
+            background-color: transparent;
+        }}
+    </style>
+    '''
+    st.markdown(css, unsafe_allow_html=True)
 
+else:
+    st.title('Gradient Background')
+    st.write('Look at the pretty shifting background')
+
+    with open('./files/wave.css') as f:
+        css = f.read()
+
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
